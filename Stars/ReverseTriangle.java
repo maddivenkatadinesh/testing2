bfe260416950c7dd5e21fcf787b9e642d2eefed6
dinesh <dineshmaddi15@gmail.com>
2024-05-05 10:32:23 +0530

    adding star programs

---
+++ Stars/ReverseTriangle.java
@@ -0,0 +1,21 @@
+package Stars;
+
+public class ReverseTriangle {
+    public static void main(String[] args) {
+        int n=5;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for (int k=0;k<n-i;k++){
+                if (k==0){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print("**");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
